f662f292cd91a05356c93a639b77105353876d50
BorysMaciuch <user.email>
2020-03-11 16:55:33 +0100

    testing buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import NavigationIcon from '@material-ui/icons/Navigation';
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    <Fab variant="extended">
+      <NavigationIcon className={classes.extendedIcon} />
+      Navigate
+    </Fab>
       </header>
     </div>
   );
