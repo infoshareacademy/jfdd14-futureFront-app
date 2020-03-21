bf26b414424536acb87d2657804df5ddf59a36da
BorysMaciuch <borysmaciuch@gmail.com>
2020-03-21 14:29:08 +0100

    form without styling added

--- src/pages/AddGift/Addgift.js
+++ src/pages/AddGift/Addgift.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import './AddGift.css'
 const Addgift = () => {
     return (
-        <div>dodaj prezent</div>
+        <Container fixed style={{backgroundColor: '#cfe8fc', height: '80vh'}}>
+            <form >
+                <h2>Dodaj swój prezent:</h2>
+            <TextField id="standard-basic" label="Nazwa prezentu" style={{paddingBottom: '3vh'}}/>
+            <TextField id="standard-basic" label="Kategoria" style={{paddingBottom: '3vh'}} />
+            <TextField id="standard-basic" label="Zdjęcie" style={{paddingBottom: '3vh'}} />
+            <TextField id="standard-basic" label="Cena" style={{paddingBottom: '3vh'}} />
+            <Button variant="contained" color="primary">Dodaj!</Button>
+            </form>
+    
+        </Container>
+        
     )
 }
 
