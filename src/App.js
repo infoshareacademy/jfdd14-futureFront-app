c8c80358065be53b8dd6232e0949c5a8560e7bb1
BorysMaciuch <borysmaciuch@gmail.com>
2020-03-30 16:00:23 +0200

    form reset and communicate added

--- src/App.js
+++ src/App.js
@@ -7,39 +7,30 @@ import Favorites from './pages/Favorites/Favorites'
 import Home from './pages/Home/Home'
 import Menu from './components/Menu/Menu_Material'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Modal from './components/Modal/Modal'
+
 
 function App() {
   const [gifts, setGift] = useState ([{name:'bb', category:'cc', photo:'dd', price:'ee', description:'ff'}]);
-   const [showModal,setModal] = useState(false) 
-   const onCloseModalHandle = () => {
-    setModal(false)
-  } 
+   
   const addGift = (gift) => {
     setGift([...gifts, gift])
     
   }
-  const modalHandle = () => {
-    setModal(true)
-  }
+  
   return (
       
       <BrowserRouter>
         <Menu>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/addgift'><Addgift addGift={addGift} modalHandle={modalHandle}/></Route> 
+          <Route path='/addgift'><Addgift addGift={addGift} /></Route> 
           <Route path='/gifts'><Giftslist gifts={gifts}/></Route>
           <Route path='/charts' component={Charts} />
           <Route path='/favorites' component={Favorites} />
         </Switch>
         
         </Menu>
-        { showModal &&
-            <Modal title='contacts' onClose={onCloseModalHandle}>
-              <div>bla bla bla</div>
-            </Modal>
-          }
+        
       </BrowserRouter>
 
   );
--- src/pages/AddGift/Addgift.js
+++ src/pages/AddGift/Addgift.js
@@ -11,22 +11,30 @@ const Addgift = (props) => {
     const [photo, setPhoto] = useState('')
     const [price, setPrice] = useState('')
     const [description, setDescription] = useState('')
+    const [giftAddedText, setGiftAddedText ] = useState(false)
   
     const addToList = () => {
         
         props.addGift({name, category, photo, price, description})
-        props.modalHandle()
+        setName('');
+        setCategory('');
+        setPhoto('');
+        setPrice('');
+        setDescription('')
+        setGiftAddedText(true)
+        setTimeout(function(){
+            setGiftAddedText(false)}, 3000)
     }
   
     
     return (
-        <Container fixed style={{backgroundColor: 'rgb(227, 188, 190)', minHeight: '80vh', padding:'2vh', boxShadow: '3px 7px 35px -4px rgba(0,0,0,0.45)', borderRadius: '2vh'}}>
+        <Container fixed style={{backgroundColor: 'rgb(227, 188, 190)', minHeight: '80vh', padding:'1vh', boxShadow: '3px 7px 35px -4px rgba(0,0,0,0.45)', borderRadius: '2vh'}}>
             <form >
                 <h2>Dodaj swój prezent:</h2>
-            <Box width="30%"><TextField fullWidth color='secondary'onChange={e => setName(e.target.value)} id="standard-basic" label="Nazwa prezentu" style={{paddingBottom: '3vh'}}/></Box>
-            <Box width="30%"><TextField fullWidth color='secondary'onChange={e => setCategory(e.target.value)}id="standard-basic" label="Kategoria" style={{paddingBottom: '3vh'}} /></Box>
-            <Box width="30%"><TextField fullWidth color='secondary'onChange={e => setPhoto(e.target.value)}id="standard-basic" label="Zdjęcie" style={{paddingBottom: '3vh'}} /></Box>
-            <Box width="30%"><TextField fullWidth color='secondary'onChange={e => setPrice(e.target.value)}id="standard-basic" label="Cena" style={{paddingBottom: '3vh'}} /></Box>
+            <Box width="30%"><TextField value={name} fullWidth color='secondary'onChange={e => setName(e.target.value)} id="standard-basic" label="Nazwa prezentu" style={{paddingBottom: '3vh'}}/></Box>
+            <Box width="30%"><TextField value={category}fullWidth color='secondary'onChange={e => setCategory(e.target.value)}id="standard-basic" label="Kategoria" style={{paddingBottom: '3vh'}} /></Box>
+            <Box width="30%"><TextField value={photo}fullWidth color='secondary'onChange={e => setPhoto(e.target.value)}id="standard-basic" label="Zdjęcie" style={{paddingBottom: '3vh'}} /></Box>
+            <Box width="30%"><TextField value={price}fullWidth color='secondary'onChange={e => setPrice(e.target.value)}id="standard-basic" label="Cena" style={{paddingBottom: '3vh'}} /></Box>
             <Box width="30%"><TextField  color='secondary' 
           id="outlined-multiline-static"
           label="Opis prezentu"
@@ -36,9 +44,12 @@ const Addgift = (props) => {
           variant='outlined'
           fullWidth
           onChange={e => setDescription(e.target.value)}
+          value={description}
         /></Box>
             <Button variant="contained" color="secondary" onClick={addToList}>Dodaj!</Button>
+            {giftAddedText && <div className='giftAdded'>Prezent dodany pomyślnie! Znajdziesz go w zakładce Gifts</div>}
             </form>
+            
     
         </Container>
         
