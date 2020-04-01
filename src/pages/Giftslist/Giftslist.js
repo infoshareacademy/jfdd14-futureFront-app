6c7bb9318b7af322a47f26c4dc6be4214690b573
BorysMaciuch <borysmaciuch@gmail.com>
2020-04-01 16:01:02 +0200

    prettier

--- src/pages/Giftslist/Giftslist.js
+++ src/pages/Giftslist/Giftslist.js
@@ -1,22 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import Container from '@material-ui/core/Container';
-import Gift from '../../components/Gift/Gift'
+import Container from "@material-ui/core/Container";
+import Gift from "../../components/Gift/Gift";
 const gift = function (props) {
-    let gifts = props.gifts
-    
-    return (
-        <Fragment>
-            <h1>
-           
-           Gift!
-        </h1>
-         <Container display="flex">{gifts ? gifts.map((e,i) => <Gift key={i} id={i} item={e}/>) : null}
-        </Container>
-         
-        </Fragment>
-        
-    );
-}
+  let gifts = props.gifts;
+
+  return (
+    <Fragment>
+      <h1>Gift!</h1>
+      <Container display="flex">
+        {gifts ? gifts.map((e, i) => <Gift key={i} id={i} item={e} />) : null}
+      </Container>
+    </Fragment>
+  );
+};
 
 export default gift;
