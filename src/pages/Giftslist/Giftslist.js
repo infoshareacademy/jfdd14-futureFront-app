4b746af9d0f083e21c9eda524ca47cf9a01dbda5
BorysMaciuch <borysmaciuch@gmail.com>
2020-04-01 16:24:56 +0200

    searching gifts functin added

--- src/pages/Giftslist/Giftslist.js
+++ src/pages/Giftslist/Giftslist.js
@@ -1,22 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from "react";
+import TextField from "@material-ui/core/TextField";
 
-import Container from '@material-ui/core/Container';
-import Gift from '../../components/Gift/Gift'
-const gift = function (props) {
-    let gifts = props.gifts
-    
-    return (
-        <Fragment>
-            <h1>
-           
-           Gift!
-        </h1>
-         <Container display="flex">{gifts ? gifts.map((e,i) => <Gift key={i} id={i} item={e}/>) : null}
-        </Container>
-         
-        </Fragment>
-        
-    );
-}
+import Container from "@material-ui/core/Container";
+import Gift from "../../components/Gift/Gift";
+const Giftlist = function (props) {
+  const [searchInput, setSearchInput] = useState("");
+  let gifts = props.gifts;
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+  return (
+    <Fragment>
+      <h1>
+        <TextField onChange={handleChange}></TextField>
+      </h1>
+      <Container display="flex">
+        {gifts
+          ? gifts
+              .filter(
+                (gift) =>
+                  gift.name
+                    .toLowerCase()
+                    .includes(searchInput.toLocaleLowerCase()) ||
+                  gift.category
+                    .toLowerCase()
+                    .includes(searchInput.toLocaleLowerCase())
+              )
+              .map((e, i) => <Gift key={i} id={i} item={e} />)
+          : null}
+      </Container>
+    </Fragment>
+  );
+};
 
-export default gift;
+export default Giftlist;
