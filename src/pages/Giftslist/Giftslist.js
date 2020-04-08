b4fd3601ecbfe816331381edc16b36f72c29a881
BorysMaciuch <borysmaciuch@gmail.com>
2020-04-08 16:22:46 +0200

    giftlist search funcion fixed

--- src/pages/Giftslist/Giftslist.js
+++ src/pages/Giftslist/Giftslist.js
@@ -57,20 +57,24 @@ const GiftList = function (props) {
         </Grid>
       </div>
 
-      {gifts.filter(
-        (gift) =>
-          gift.name.toLowerCase().includes(searchInput.toLocaleLowerCase()) ||
-          gift.category.toLowerCase().includes(searchInput.toLocaleLowerCase())
-      ).length > 0 &&
+      {gifts
+        ?.slice(page * giftsPerPage, page * giftsPerPage + giftsPerPage)
+        .filter(
+          (gift) =>
+            gift.name.toLowerCase().includes(searchInput.toLocaleLowerCase()) ||
+            gift.category
+              .toLowerCase()
+              .includes(searchInput.toLocaleLowerCase())
+        ).length > 0 &&
       gifts
-        .slice(page * giftsPerPage, page * giftsPerPage + giftsPerPage)
+        ?.slice(page * giftsPerPage, page * giftsPerPage + giftsPerPage)
         .filter(
           (gift) =>
             Number(gift.price) >= Number(sliderInput[0]) &&
             Number(gift.price) < Number(sliderInput[1])
         ).length > 0 ? (
         gifts
-          .slice(page * giftsPerPage, page * giftsPerPage + giftsPerPage)
+          ?.slice(page * giftsPerPage, page * giftsPerPage + giftsPerPage)
           .filter(
             (gift) =>
               gift.name
@@ -80,7 +84,7 @@ const GiftList = function (props) {
                 .toLowerCase()
                 .includes(searchInput.toLocaleLowerCase())
           )
-          .slice(page * giftsPerPage, page * giftsPerPage + giftsPerPage)
+          ?.slice(page * giftsPerPage, page * giftsPerPage + giftsPerPage)
           .filter(
             (gift) =>
               Number(gift.price) >= Number(sliderInput[0]) &&
